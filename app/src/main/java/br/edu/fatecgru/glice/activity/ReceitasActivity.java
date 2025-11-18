38bf4d3b231edb0f2f3a9414f7eb2190c084171e
christoferoAngelo <christoferoangelo@gmail.com>
2025-11-18 14:44:04 -0300

    telas sem nada bonito

---
+++ app/src/main/java/br/edu/fatecgru/glice/activity/ReceitasActivity.java
@@ -0,0 +1,26 @@
+package br.edu.fatecgru.glice.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.edu.fatecgru.glice.R;
+
+public class ReceitasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_receitas);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
